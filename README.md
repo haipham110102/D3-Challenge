81d3174c7f89f67a1059c8efc9bfda30de37bc7d
hpham110102 <hpham110102@gmail.com>
2022-01-06 20:48:11 -0600

    orgizing files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # D3-Challenge
+- Correlations Discovered Between Health Risks and Age, Income
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 // SVG wrapper dimensions are determined by the current width
 // and height of the browser window.
-const svgWidth = 920;
-const svgHeight = 620;
+const svgWidth = 960;
+const svgHeight = 600;
 
-// Define the chart's margins as an object 
+// Define dimensions of the chart area
 const margin = {
     top: 20,
     right: 40,
@@ -35,19 +35,17 @@ d3.csv("assets/data/data.csv").then((stateData) => {
     });
 
     // Step 2: Create scale functions
-    const xScale = d3.scaleLinear()
-        .domain([d3.min(stateData, d => d.poverty) * 0.85,
-        d3.max(stateData, d => d.poverty) * 1.25])
+    const xLinearScale = d3.scaleLinear()
+        .domain([d3.min(stateData, d => d.poverty) * 0.8, d3.max(stateData, d => d.poverty) * 1.2])
         .range([0, width]);
 
-    const yScale = d3.scaleLinear()
-        .domain([d3.min(stateData, d => d.healthcare) * 0.85,
-        d3.max(stateData, d => d.healthcare) * 1.25])
+    const yLinearScale = d3.scaleLinear()
+        .domain([d3.min(stateData, d => d.healthcare) * 0.8, d3.max(stateData, d => d.healthcare) * 1.2])
         .range([height, 0]);
 
     // Step 3: Create axis functions
-    const bottomAxis = d3.axisBottom(xScale);
-    const leftAxis = d3.axisLeft(yScale);
+    const bottomAxis = d3.axisBottom(xLinearScale);
+    const leftAxis = d3.axisLeft(yLinearScale);
 
     // Step 4: Append Axes to the chart
     chartGroup.append("g")
@@ -57,33 +55,33 @@ d3.csv("assets/data/data.csv").then((stateData) => {
     chartGroup.append("g")
         .call(leftAxis);
 
-    // Step 5: Create Circles
+    // Step 5a: Create Circles
     const circlesGroup = chartGroup.selectAll(".stateCircle")
         .data(stateData)
         .enter()
         .append("circle")
-        .attr("cx", d => xScale(d.poverty))
-        .attr("cy", d => yScale(d.healthcare))
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
         .attr("class", "stateCircle")
-        .attr("r", "15")
-        .attr("opacity", ".8");
+        .attr("r", "14")
+        .attr("opacity", ".7");
 
-    // Create circle text
+    // Step 5b:Create circle text
     const circlesText = chartGroup.selectAll(".stateText")
         .data(stateData)
         .enter()
         .append("text")
-        .attr("x", d => xScale(d.poverty))
-        .attr("y", d => yScale(d.healthcare * .99))
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.healthcare * .99))
         .text(d => d.abbr)
         .attr("class", "stateText")
-        .attr("font-size", "12px");
+        .attr("font-size", "10px");
 
     // Step 6: Initialize tool tip
     const toolTip = d3.tip()
         .attr("class", "tooltip d3-tip")
-        .offset([85, 85])
-        .html((d) => {
+        .offset([90, -60])
+        .html((d) => { 
             return (`${d.state}<br>Poverty: ${d.poverty}%<br>Healthcare: ${d.healthcare}%`);
         });
 
@@ -91,20 +89,19 @@ d3.csv("assets/data/data.csv").then((stateData) => {
     chartGroup.call(toolTip);
 
     // Step 8: Create event listeners to display and hide the tooltip
-    circlesGroup.on("mouseenter", function (data) {
+    circlesGroup.on("mouseenter", function(data) { 
         toolTip.show(data, this);
     })
-        // onmouseout event
-        .on("mouseout", function (data) {
-            toolTip.hide(data);
-        });
+    .on("mouseout", function(data) {
+        toolTip.hide(data);
+    });
 
-    circlesText.on("mouseenter", function (data) {
+    circlesText.on("mouseenter", function(data) {
         toolTip.show(data, this);
     })
-        .on("mouseout", function (data) {
-            toolTip.hide(data);
-        });
+    .on("mouseout", function(data) {
+        toolTip.hide(data);
+    });
 
     // Create axes labels
     chartGroup.append("text")
@@ -112,14 +109,14 @@ d3.csv("assets/data/data.csv").then((stateData) => {
         .attr("y", 0 - margin.left + 40)
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
-        .attr("class", "axisText")
+        .attr("class", "aText")
         .text("Lacks Healthcare(%)");
 
     chartGroup.append("text")
         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-        .attr("class", "axisText")
+        .attr("class", "aText")
         .text("In Poverty (%)");
 })
-    .catch((error) => {
-        console.log(error);
-    });
+.catch((error) => { 
+      console.log(error);
+});
